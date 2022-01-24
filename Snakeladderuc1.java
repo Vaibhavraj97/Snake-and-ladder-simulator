cd9431b094346998a6ffd00eb2f4873e80d535ee
vaibhav rajuskar <vaibhavrajuskar54@gmail.com>
2022-01-24 08:54:46 +0530

    single player at strt posn 0

---
+++ Snakeladderuc1.java
@@ -0,0 +1,15 @@
+package Simulatorsnakeladder;
+
+public class Snakeladderuc1 {
+	
+		 static int startPosn = 0;
+		 static int newPosition = startPosn;
+		  int playerOne=0;
+		  
+		  public static void main(String[] args) {
+
+	          System.out.println("Starting Position : "+ startPosn);
+
+	}
+	}
+
